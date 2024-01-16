e700db6ddb606a5f3b118529d80d85914258729e
objektwerks <objektwerks@runbox.com>
2024-01-16 08:40:21 -0500

    enhanced hello

--- hello.swift
+++ hello.swift
@@ -10,3 +10,6 @@ print("*** Hello, Swift! ***\n")
 print("*** Array ***\n")
 let ints = [1, 2, 3]
 print("ints: \(ints) sum to: \(ints.reduce(0, +))")
+
+print("*** Dictionary ***\n")
+let map = [1:1, 2:2, 3:3]
