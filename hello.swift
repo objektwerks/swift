21a7f6f8664079c89335bae0f0b6fc1b3df4f9fa
objektwerks <objektwerks@runbox.com>
2024-01-16 08:43:42 -0500

    edited hello

--- hello.swift
+++ hello.swift
@@ -5,13 +5,13 @@ import Foundation
   To include basic language features.
 */
 
-print("*** Hello, Swift! ***\n")
+print("*** Hello, Swift! ***")
 
-print("*** Array ***\n")
+print("\n*** Array ***")
 let ints = [1, 2, 3]
 print("ints: \(ints) sum to: \(ints.reduce(0, +))")
 
-print("*** Dictionary ***\n")
+print("\n*** Dictionary ***")
 let map = [1:1, 2:2, 3:3]
 print("map: \(map) sum keys to: \(map.keys.reduce(0, +))")
 print("map: \(map) sum values to: \(map.values.reduce(0, +))")
